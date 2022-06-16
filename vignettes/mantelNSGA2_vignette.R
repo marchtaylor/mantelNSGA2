bb09149d0b455f1ecf3abbe8303c478a0dfd0877
Marc Taylor <marchtaylor@gmail.com>
2022-06-16 16:25:33 +0200

    description updated

---
+++ vignettes/mantelNSGA2_vignette.R
@@ -0,0 +1,11 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  cache = TRUE, # TRUE for speed. Remember to empty cache after changes to cached R code sections
+  cache.path = "cache/", fig.path = "tex/",
+  echo = TRUE, 
+  collapse = TRUE,
+  comment = "#>",
+  fig.width = 6, fig.height = 5, dpi=300, 
+  out.width="600px", out.height="500px"
+)
+
